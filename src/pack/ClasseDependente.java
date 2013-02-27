8e20b2e448b648b6aff1a2a202ef852b6ebef99f
Jefferson <jefferson.almeida.comp@gmail.com>
2013-02-27 09:05:21 -0300

    First Commit - Soot Framework

---
+++ src/pack/ClasseDependente.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class ClasseDependente {
+	
+	private Teste teste;
+	
+	public ClasseDependente() {
+		this.teste = new Teste();
+	}
+	
+	public Parametro metodo(Parametro parametro){
+		return null;
+	}
+	
+	
+
+}
